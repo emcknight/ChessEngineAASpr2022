e665857b89e4a2139d22e320e5ea4f6680107c72
StoneKnight117 <bkstonehill99@gmail.com>
2022-03-16 13:48:38 -0400

    Added the alpha beta algorithm to the project.

--- Search/Search.py
+++ Search/Search.py
@@ -5,7 +5,7 @@
 # Version:      1.0
 
 import chess
-from Utilities.SearchUtils import negamaxUtil
+from Utilities.SearchUtils import negamaxUtil, alphaBetaUtil
 
 
 def negamax(board: chess.Board, depth: int, evaluation):
@@ -22,9 +22,33 @@ def negamax(board: chess.Board, depth: int, evaluation):
     selected_move = None
     for move in board.legal_moves:
         board.push(move)
-        score = -negamaxUtil(move)
+        score = negamaxUtil(board, depth-1, evaluation)
         board.pop()
         if score > maximum:
             maximum = score
             selected_move = move
     return selected_move
+
+
+def alphaBeta(board: chess.Board, depth: int, evaluation):
+    """
+    Turn based tree search algorithm based on the negamax algorithm and reduces the search tree by pruning leaves
+    according to alpha and beta values. As it is based on negamax, the evaluation function must return a score relative
+    to the side being evaluated.
+    :param board: The board to evaluate and make temporary moves on.
+    :param depth: The maximum depth to traverse.
+    :param evaluation: The evaluation function to perform on the board.
+    :return: The best scoring move based on the evaluation scores.
+    """
+
+    maximum = float('-inf')
+    selected_move = None
+    for move in board.legal_moves:
+        board.push(move)
+        score = alphaBetaUtil(board, float('-inf'), float('inf'), depth-1, evaluation)
+        board.pop()
+        if score > maximum:
+            maximum = score
+            selected_move = move
+    return selected_move
+
--- Utilities/SearchUtils.py
+++ Utilities/SearchUtils.py
@@ -13,7 +13,7 @@ def negamaxUtil(board: chess.Board, depth: int, evaluation):
     :param board: The board to evaluate and make temporary moves on.
     :param depth: The max depth to traverse.
     :param evaluation: The evaluation function to perform on the board at depth = 0.
-    :return: The maximum score from the search tree.
+    :return: The maximum score from the subtree.
     """
 
     if depth == 0 or board.outcome() is not None:
@@ -27,3 +27,30 @@ def negamaxUtil(board: chess.Board, depth: int, evaluation):
         if score > maximum:
             maximum = score
     return maximum
+
+
+def alphaBetaUtil(board: chess.Board, alpha: float, beta: float, depth: int, evaluation):
+    """
+    Recursive utility function for the root alphaBeta function.
+    :param board: The board to evaluate and make temporary moves on.
+    :param alpha: The minimum score for the maximizing player
+    :param beta: The maximum score for the minimizing player
+    :param depth: The max depth to traverse.
+    :param evaluation: The evaluation function to perform on the board.
+    :return: The maximum score from the subtree
+    """
+
+    if depth == 0 or board.outcome() is not None:
+        evaluation(board)
+
+    for move in board.legal_moves:
+        board.push(move)
+        score = -alphaBetaUtil(board, -beta, -alpha, depth-1, evaluation)
+        board.pop()
+        if score >= beta:
+            return beta
+        if score > alpha:
+            alpha = score
+    return alpha
+
+
