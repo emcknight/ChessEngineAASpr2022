9d2ef6e6762921ec85080928025e5612e094929c
Tucker Yazdani <84822334+tuckeryazdani@users.noreply.github.com>
2022-03-23 16:42:23 -0400

    Update evaluation.py

--- Evaluate/evaluation.py
+++ Evaluate/evaluation.py
@@ -1,14 +1,17 @@
-#Author:        Tucker Yazdani
+# Project:      ChessEngineAASpr2022
+# Author:       Tucker Yazdani
+# Date:         03/16/2022
+# Last Updated: 03/23/2022
+# Version:      1.1
+
+# Author:        Tucker Yazdani
 # Date:         03/18/2022
 import chess
-
-# ToDo: Ask team how they would like me to return the score.
-
 # Disclaimer: I did not come up with the evaluation criteria values myself. I only created the code to utilize it.
 # Source for evaluation criteria:
 # https://www.chessprogramming.org/Simplified_Evaluation_Function
 
-def evalPawn(moveToIndex):
+def evalPawnWhite(moveToIndex):
     'Returns evaluation score for where a Pawn is best on the board'
     posEval=[
                 0,  0,  0,  0,  0,  0,  0,  0,
@@ -22,7 +25,7 @@ def evalPawn(moveToIndex):
     ]
     return posEval[moveToIndex]
 
-def evalKnight(moveToIndex):
+def evalKnightWhite(moveToIndex):
     'Returns evaluation score for where a Knight is best on the board'
     posEval=[
                 -50,-40,-30,-30,-30,-30,-40,-50,
@@ -36,7 +39,7 @@ def evalKnight(moveToIndex):
     ]
     return posEval[moveToIndex]
 
-def evalRook(moveToIndex):
+def evalRookWhite(moveToIndex):
     'Returns evaluation score for where a Rook is best on the board'
     posEval=[
                  0,  0,  0,  0,  0,  0,  0,  0,
@@ -50,7 +53,7 @@ def evalRook(moveToIndex):
     ]
     return posEval[moveToIndex]
 
-def evalBishop(moveToIndex):
+def evalBishopWhite(moveToIndex):
     'Returns evaluation score for where a Bishop is best on the board'
     posEval=[
                 -20,-10,-10,-10,-10,-10,-10,-20,
@@ -64,7 +67,7 @@ def evalBishop(moveToIndex):
         ]
     return posEval[moveToIndex]
 
-def evalQueen(moveToIndex):
+def evalQueenWhite(moveToIndex):
     'Returns evaluation score for where a Queen is best on the board'
     posEval=[
             -20,-10,-10, -5, -5,-10,-10,-20,
@@ -78,7 +81,7 @@ def evalQueen(moveToIndex):
         ]
     return posEval[moveToIndex]
 
-def evalKing(moveToIndex):
+def evalKingWhite(moveToIndex):
     'Returns evaluation score for where a King is best on the board'
     posEval=[
                 -30,-40,-40,-50,-50,-40,-40,-30,
@@ -95,31 +98,39 @@ def evalKing(moveToIndex):
 def evalType(pieceType,moveFromIndex):
     'Activates a function to find an evaluation score for a piece type\'s position on the board'
     if pieceType.upper()=='P':
-        return evalPawn(moveFromIndex)
+        return evalPawnWhite(moveFromIndex)
     elif pieceType.upper()=='N':
-        return evalKnight(moveFromIndex)
+        return evalKnightWhite(moveFromIndex)
     elif pieceType.upper()=='B':
-        return evalBishop(moveFromIndex)
+        return evalBishopWhite(moveFromIndex)
     elif pieceType.upper()=='R':
-        return evalRook(moveFromIndex)
+        return evalRookWhite(moveFromIndex)
     elif pieceType.upper()=='Q':
-        return evalQueen(moveFromIndex)
+        return evalQueenWhite(moveFromIndex)
     elif pieceType.upper()=='K':
-        return evalKing(moveFromIndex)
+        return evalKingWhite(moveFromIndex)
+    else:
+        return 0
 
 def evalCapture(capturedType):
     'Returns a score for capturing different pieces on the board that are valued differently'
     if capturedType.upper()=='P':
+        print('Pawn Captured')
         return 100
-    elif capturedType.upper()=='P':
+    elif capturedType.upper()=='n':
+        print('Knight Captured')
         return 320
     elif capturedType.upper()=='B':
+        print('Bishop Captured')
         return 330
     elif capturedType.upper()=='R':
+        print('Rook Captured')
         return 500
     elif capturedType.upper()=='Q':
+        print('Queen Captured')
         return 900
     elif capturedType.upper()=='K':
+        print('Check Mate')
         return 20000
     else:
         return 0
@@ -130,50 +141,52 @@ def evalBlunder(board,moveToIndex,pieceType):
     if board.attackers(chess.WHITE,moveToIndex) is not None:
         return -evalCapture(pieceType)
     
-def evaluateScore(pieceType,capturedType,moveFromIndex,moveToIndex):
+def evaluateScore(board,chessToIndex):
     'Aggregates the total score from evalCapture and evalType'
+    # Set "move" to the latest move
+    move = board.move_stack[-1]
+
+    # Set moveFromSquare to the original position of piece before movement. Set moveToSquare to the original piece after movement. Cast into a string for both.
+    # Note: Move is an object in form of "a2a3". Where a2 is original position and a3 is ending position.
+    moveFromSquare=str(move)[:2]
+    moveToSquare=str(move)[2:]
+
+    # Change the string moveFromIndex/moveToIndex to an integer to be compatible with methods in the library. i.e (a1 = 0, a=7,...)
+    moveFromIndex=chessToIndex[moveFromSquare]
+    moveToIndex=chessToIndex[moveToSquare]
+
+    # Undo move to check if there was a piece captured before move was made (String capturedType) then push the move back to the board.
+    board.pop()
+    capturedType=str(board.piece_at((chessToIndex[moveToSquare])))
+    board.push(move)
+
+    # Get piece type of the piece that was just moved. Cast into a string.
+    pieceType=str(board.piece_at(chessToIndex[moveFromSquare]))
+
+    print(evalType(pieceType,moveFromIndex))
+    print(evalBlunder(board,moveToIndex,pieceType))
     score=evalCapture(capturedType)+evalType(pieceType,moveFromIndex)+evalBlunder(board,moveToIndex,pieceType)
     return score
 
 # Chess location to index dictionary.
 chessToIndex={
-        'a1':0,'a2':1,'a3':2,'a4':3,'a5':4,'a6':5,'a7':6,'a8':7,
-        'b1':8,'b2':9,'b3':10,'b4':11,'b5':12,'b6':13,'b7':14,'b8':15,
-        'c1':16,'c2':17,'c3':18,'c4':19,'c5':20,'c6':21,'c7':22,'c8':23,
-        'd1':24,'d2':25,'d3':26,'d4':27,'d5':28,'d6':29,'d7':30,'d8':31,
-        'e1':32,'e2':33,'e3':34,'e4':35,'e5':36,'e6':37,'e7':38,'e8':39,
-        'f1':40,'f2':41,'f3':42,'f4':43,'f5':44,'f6':45,'f7':46,'f8':47,
-        'g1':48,'g2':49,'g3':50,'g4':51,'g5':52,'g6':53,'g7':54,'g8':55,
-        'h1':56,'h2':57,'h3':58,'h4':59,'h5':60,'h6':61,'h7':62,'h8':63
+        'a8':56,'b8':57,'c8':58,'d8':59,'e8':60,'f8':61,'g8':62,'h8':63,
+        'a7':48,'b7':49,'c7':50,'d7':51,'e7':52,'f7':53,'g7':54,'h7':55,
+        'a6':40,'b6':41,'c6':42,'d6':43,'e6':44,'f6':45,'g6':46,'h6':47,
+        'a5':32,'b5':33,'c5':34,'d5':35,'e5':36,'f5':37,'g5':38,'h5':39,
+        'a4':24,'b4':25,'c4':26,'d4':27,'e4':28,'f4':29,'g4':30,'h4':31,
+        'a3':16,'b3':17,'c3':18,'d3':19,'e3':20,'f3':21,'g3':22,'h3':23,
+        'a2':8, 'b2':9, 'c2':10,'d2':11,'e2':12,'f2':13,'g2':14,'h2':15,
+        'a1':0, 'b1':1, 'c1':2, 'd1':3, 'e1':4, 'f1':5, 'g1':6, 'h1':7
 }
 
-# Begin Example
-
 board=chess.Board()
-# Randomly chosen move. This move is illegal and only for demonstration. Chosen because this example will be a new board and opposing side will be far away.
-chosenMove='a1g2'
 
-# Takes Move object in form of "a1a2" where a1 is the starting position and a2 is the ending position.
-# Cast it into a string to use chessToIndex sepearting "To" and "From" variables.
-moveFromSquare=str(chosenMove)[:2]
-moveToSquare=str(chosenMove)[2:]
+for mv in board.legal_moves:
+    move=mv
 
-moveFromIndex=chessToIndex[moveFromSquare]
-moveToIndex=chessToIndex[moveToSquare]
+board.push(move)
 
-# Gets the type of piece located at moveFrom and Gets the the type of piece if any at moveTo.
-pieceType=str(board.piece_at(chessToIndex[moveFromSquare]))
-capturedType=str(board.piece_at((chessToIndex[moveToSquare])))
+print(evaluateScore(board,chessToIndex))
 
-# Prints example evaluation score.
-print(evaluateScore(pieceType,capturedType,moveFromIndex,moveToIndex))
-
-# End Example
-
-# Blunder Testing
-board.push(chess.Move.from_uci('b2b5'))
-board.push(chess.Move.from_uci('a8b6'))
-moveToIndex=41
 print(board)
-
-print(evalBlunder(board,moveToIndex,pieceType))
